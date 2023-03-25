2a18aa74b731ea855c8c90281a426df5dd4918ce
Natalí Rodrigues <101203099+Natalirodriguess@users.noreply.github.com>
2023-03-25 19:53:07 -0300

    Update HelloWorldSelenium.java

--- 2019-selenium-java-projeto_inicial/src/test/java/HelloWorldSelenium.java
+++ 2019-selenium-java-projeto_inicial/src/test/java/HelloWorldSelenium.java
@@ -7,7 +7,6 @@ public class HelloWorldSelenium {
 	@Test
 	public void helloWorld() {
 		
-		
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://localhost:8080/leiloes");
