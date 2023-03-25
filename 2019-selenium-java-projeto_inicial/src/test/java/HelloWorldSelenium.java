887b4e59641a2e147ccdc3cce86b6d398e32cec4
Natalí Rodrigues <101203099+Natalirodriguess@users.noreply.github.com>
2023-03-25 19:52:05 -0300

    Update HelloWorldSelenium.java

--- 2019-selenium-java-projeto_inicial/src/test/java/HelloWorldSelenium.java
+++ 2019-selenium-java-projeto_inicial/src/test/java/HelloWorldSelenium.java
@@ -7,11 +7,9 @@ public class HelloWorldSelenium {
 	@Test
 	public void helloWorld() {
 		
-		//mostrar onde está meu drive do navegador
+		
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-		//abrir o navegador a partir da implementação da interface WebDriver com a classe ChromeDriver
 		WebDriver driver = new ChromeDriver();
-		//ao abrir o navegador , vá para:
 		driver.navigate().to("http://localhost:8080/leiloes");
 		driver.quit();
 	}
